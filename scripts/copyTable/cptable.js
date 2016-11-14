0bf781d8d4434881ea2d1631f5f7fc468320ee05
Ian Laird <irlaird@velma.com>
2016-11-14 11:44:07 -0700

    Added a CopyTable script

---
+++ scripts/copyTable/cptable.js
@@ -0,0 +1,139 @@
+#!/usr/bin/env node
+'use strict';
+
+const AWS = require('aws-sdk');
+const async = require('async');
+const ArgumentParser = require('argparse').ArgumentParser;
+const packageJSON = require('./package.json');
+
+function getOpts() {
+    var ap = new ArgumentParser({
+        version: packageJSON.version,
+        addHelp: true,
+        description: packageJSON.description
+    });
+
+    ap.addArgument([ 'SOURCE' ], {
+        help: 'The name of the table to copy.',
+        type: String
+    });
+
+    ap.addArgument([ 'TARGET' ], {
+        help: 'The name of the table to create.',
+        type: String
+    });
+
+    ap.addArgument([ '--region' ], {
+        help: 'Override the region to connect to.',
+        type: String,
+        defaultValue: 'us-west-2'
+    });
+
+    ap.addArgument([ '--read-capacity' ], {
+        help: 'Override the read capacity for all indexes on the new table.',
+        type: Number,
+        defaultValue: null
+    });
+
+    ap.addArgument([ '--write-capacity' ], {
+        help: 'Override the write capacity for all indexes on the new table.',
+        type: Number,
+        defaultValue: null
+    });
+
+    return ap.parseArgs();
+}
+
+function main(opts) {
+    var ddb = new AWS.DynamoDB({ region: opts.region });
+
+    var tasks = [
+        loadTableDescription,
+        createNewTableDescription,
+        createNewTable
+    ];
+
+    async.waterfall(tasks, (err, data) => {
+        if(err) {
+            console.error(err);
+            process.exit(1);
+        } else {
+            console.info(data);
+        }
+
+    });
+
+    function loadTableDescription(done) {
+        console.info("Loading table description: " + opts.SOURCE);
+        ddb.describeTable({ TableName: opts.SOURCE }, (err, data) => {
+            if(!err)
+                opts.sourceDescription = data.Table;
+            done(err);
+        });
+    }
+
+    function createNewTableDescription(done) {
+        console.info("Creating new table description");
+
+        opts.targetDescription = {
+            TableName: opts.TARGET,
+            AttributeDefinitions: opts.sourceDescription.AttributeDefinitions,
+            KeySchema: opts.sourceDescription.KeySchema,
+            ProvisionedThroughput: {
+                ReadCapacityUnits: opts.read_capacity || opts.sourceDescription.ProvisionedThroughput.ReadCapacityUnits,
+                WriteCapacityUnits: opts.write_capacity || opts.sourceDescription.ProvisionedThroughput.WriteCapacityUnits,
+            }
+        };
+
+        if('GlobalSecondaryIndexes' in opts.sourceDescription) {
+
+            opts.targetDescription.GlobalSecondaryIndexes = [];
+
+            for(var i = 0; i < opts.sourceDescription.GlobalSecondaryIndexes.length; i++) {
+                var gsi = opts.sourceDescription.GlobalSecondaryIndexes[i];
+                opts.targetDescription.GlobalSecondaryIndexes.push({
+                    IndexName: gsi.IndexName,
+                    KeySchema: gsi.KeySchema,
+                    Projection: gsi.Projection,
+                    ProvisionedThroughput: {
+                        ReadCapacityUnits: opts.read_capacity || gsi.ProvisionedThroughput.ReadCapacityUnits,
+                        WriteCapacityUnits: opts.write_capacity || gsi.ProvisionedThroughput.WriteCapacityUnits
+                    }
+                });
+            }
+        }
+
+        if('LocalSecondaryIndexes' in opts.sourceDescription) {
+
+            opts.targetDescription.LocalSecondaryIndexes = [];
+
+            for(var i = 0; i < opts.sourceDescription.LocalSecondaryIndexes.length; i++) {
+                var lsi = opts.sourceDescription.LocalSecondaryIndexes[i];
+                opts.targetDescription.LocalSecondaryIndexes.push({
+                    IndexName: lsi.IndexName,
+                    KeySchema: lsi.KeySchema,
+                    Projection: lsi.Projection,
+                    ProvisionedThroughput: {
+                        ReadCapacityUnits: opts.read_capacity || lsi.ProvisionedThroughput.ReadCapacityUnits,
+                        WriteCapacityUnits: opts.write_capacity || lsi.ProvisionedThroughput.WriteCapacityUnits
+                    }
+                });
+            }
+        }
+
+        done();
+    }
+
+    function createNewTable(done) {
+        console.info("Creating new table: " + opts.TARGET);
+
+        ddb.createTable(opts.targetDescription, (err, data) => {
+            if(err)
+                return done(err);
+
+            done("Completed Successfully!");
+        });
+    }
+}
+
+main(getOpts());
