f555cb9e529ed2040b44cf5415e53761960f7bb5
kunningklown@gmail.com <kunningklown@gmail.com>
2016-12-19 14:29:45 -0700

    uploading current status of script

--- scripts/S3LibraryMove/bin/s3Move.js
+++ scripts/S3LibraryMove/bin/s3Move.js
@@ -7,7 +7,7 @@ const async = require('async');
 const ArgumentParser = require('argparse').ArgumentParser;
 const packageJSON = require('../package.json');
 const request = require('request');
-const sleep = require('sleep');
+const filesToGet = ['/thumb.jpg','/template.html','/preview.jpg'];
 
 
 
@@ -19,10 +19,17 @@ function getOpts(){
 		description: packageJSON.description
     });
 	
-	/* ap.addArgument(['CMDPKT'], {
-		help: 'Path to command packet',
-		type: String
-	}); */
+	ap.addArgument(['--ptable'], {
+		help: 'name of product table to pull data from',
+		type: String,
+		defaultValue: 'TEST_VFS_Product'
+	});
+	
+	ap.addArgument(['--bktsubdir'], {
+		help: 'name of bucket subdirectory productid directories are stored in',
+		type: String,
+		defaultValue: 'test'
+	})
 
 		
 	return ap.parseArgs();
@@ -31,7 +38,9 @@ function getOpts(){
 function main(opt){
 	
 	var tasks = [
-		getS3dirs 
+		getDynamoProdInfo,
+		getS3Dirs
+		
 		
 	];
 	
@@ -58,38 +67,128 @@ function main(opt){
 		
 		this info will be used to updated the related vfs tables with the new path
 		-also update the item id with the prod id
+		library.velma.com/test library.velma.com/prod library.velma.com/dev
 	*/	
-	function getS3dirs(){
-		
-		var s3 = new AWS.S3();
-		
-		/* var params = {
-			Bucket: 'vfs-library',
-			Key: 'test/1000/'
-		}
-		
-		s3.getObject(params, function(err, data){
-			if(err) console.log(err, err.stack);
-			else console.log(data.Body);
-			
-		}); */
+	
+	//this gets the objects from the dynamo product table
+	function getDynamoProdInfo(done){
+		opt.productIdArry = [];
+		var ddb = new AWS.DynamoDB({ region: 'us-west-2' });
 		
 		var params = {
-			Bucket: 'vfs-library',
-			Marker: 'test'
-			
-		}
+			TableName: opt.ptable
 		
-		s3.listObjects(params, function(err, data){
-			if(err) console.log(err, err.stack);
-			else console.log(data);
+			
+		};
+		ddb.scan(params, function(err, data){
+			if(err){
+				console.log(err, err.stack);
+			}else {
+				//console.log(data.Items[0].id);
+				opt.products = data.Items;
+				
+				//console.log(opt.products);
+				for(var product in data.Items){
+					var idToInt = parseInt();
+					//console.log(data.Items[product].productId.S);
+					//create array of product ids and exclude any that are not 1000 series
+					if(data.Items[product].productId.S.includes("10") ){ 
+						opt.productIdArry.push(data.Items[product].productId.S);
+					
+					}
+					
+					
+				}
+				//console.log(opt.productIdArry);
+				
+				
+			}
 			
 		});
-		
+		done();
 	}
+	
+//this will get a list of objects in a specific s3 directory
+function getS3Dirs(done){
+	
+	var s3 = new AWS.S3();
+		
+	//needs to be restricted by highest number
+	for(var count = 1001; count < 1006; count++){
+		for (var file in filesToGet){
+			
+			var key = count.toString();
+			var params = {
+				Bucket: 'vfs-library'
+				,Key: opt.bktsubdir + '/' + key + filesToGet[file]
+				//,Key: 'test/1006/thumb.jpg'
+				
+			}
+			//console.log(params);
+
+			s3.getObject(params, function(err, data){
+				if(err) console.log(err, err.stack);
+				else{
+					//console.log(data);
+					//old path https://fs.velma.com/library/product/1005/thumb.jpg?versionId=wFSbEdich0b0s0EwiWUtFer.Hz27DvrV
+					//new path https://as.velma.com/vfs_library/1005/thumb.jpg?versionId=wFSbEdich0b0s0EwiWUtFer.Hz27DvrV
+					var shortFileToGet = filesToGet[file].replace("test/","");
+					console.log(filesToGet[file]);
+					if (filesToGet[file].includes("thumb")){
+						var newThumbPath = "https://as.velma.com/vfs_library" + shortFileToGet + "?versionId=" + data.VersionId;
+						console.log(newThumbPath);
+					}
+					if (filesToGet[file].includes("preview")){
+						var newPreviewPath = "https://as.velma.com/vfs_library" + shortFileToGet + "?versionId=" + data.VersionId;
+						//console.log(newPreviewPath);
+					}
+					if (filesToGet[file].includes("template")){
+						var newTemplatePath = "https://as.velma.com/vfs_library" + shortFileToGet + "?versionId=" + data.VersionId;
+						//console.log(newTemplatePath);
+					}
+					
+					for(var product in opt.products){
+						
+						if(opt.products[product].productId.S == key){
+							var productPaths = [{
+								id : opt.products[product].id.S,
+								productId: opt.products[product].productId.S,
+								thumb: newThumbPath,
+								preview: newPreviewPath,
+								template: newTemplatePath
+								
+								
+								
+							}];
+ 
+							//console.log(productPaths);
+							
+						}
+						
+					}
+					
+				
+				} 
+				
+				
+			});
+			
+		}
+
+	}
+	 done();
+	
+}
+	
+	
+	
+	
+	
+
 		
 		
 
 }
+
 
 main(getOpts());
